3f68a720c60ab1c13cbc3546ef55603eda6746e5
Josh <herz.josh@gmail.com>
2019-11-05 10:59:14 -0800

    wrote boilerplate code for index.jsx and app.jsx

---
+++ client/src/Components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './components/sidebar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Testing App
+        <div>Rendering Sidebar Component
+        <Sidebar></Sidebar>
+        </div>
+      </div>
+
+    )
+  }
+}
+export default App;

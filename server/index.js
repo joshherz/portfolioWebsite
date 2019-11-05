bb4dff7fc66ee3dc500bd0e6ae57e6ca8333f041
Josh <herz.josh@gmail.com>
2019-11-04 17:53:21 -0800

    Added basic file structure with index.html, server index.js, and gitignore for node modules

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../db/index.js');
+const port = 3010;
+const app = express();
+const bodyParser = require('body-parser');
+
+//app.use('/', express.static('public'));
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(port, ()=> {console.log(`listening on port number: ${port}`)});
+
+// app.get('/portfolio/api', (req,res) => {
+//   res.send('Hello World. This is my portfolio website');
+// });

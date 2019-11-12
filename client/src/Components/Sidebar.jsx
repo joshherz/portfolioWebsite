58ea5ccdc3e73317037692155ef2709417cbc369
Josh <herz.josh@gmail.com>
2019-11-12 10:38:48 -0800

    coding out Sidebar component

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 class Sidebar extends React.Component{
   render() {
     return (
-      <div>Hi, I'm Sidebar</div>
+      <Menu>
+        <a className="menu-item" href="#chemvr">
+        ChemVR
+        </a>
+        <a className="menu-item" href="#ochemxr">
+        OchemXR
+        </a>
+        <a className="menu-item" href="#collaborate">
+        Collaborate With Me
+        </a>
+      </Menu>
     )
   }
 }

c0d0e7f2215aa792cc1300ca3fdf840945e5a6d3
Josh <herz.josh@gmail.com>
2019-11-11 20:51:20 -0800

    finished defining compatibleWith function in ProjectList Component

--- client/src/Components/ProjectList.jsx
+++ client/src/Components/ProjectList.jsx
@@ -4,10 +4,25 @@ class ProjectList extends React.Component {
   constructor(props){
     this.state = {};
     this.compatibleWith = this.compatibleWith.bind(this);
+    this.oculusSVG = "https://joshportfoliowebsite.s3-us-west-1.amazonaws.com/oculus.svg";
+    this.valveIndexSVG = "https:joshportfoliowebsite.s3-us-west-1.amazonaws.com/valveIndex2.svg";
+    this.hololensSVG = "https://joshportfoliowebsite.s3-us-west-1.amazonaws.com/hololens.svg";
+
   }
   compatibleWith(/*takes boolean value*/){
       //if true, returns svg icon of vr/ar headset project is compatible with
-      return('placeholder');
+     if(compatibleWithOculus){
+       return(
+         <div>
+           <img src={this.oculusSVG}></img>
+           <img src={this.valveIndexSVG}></img>
+         </div>
+       )
+     } else{
+       return(
+         <img src={this.hololensSVG}></img>
+       )
+     }
   }
 
   render(){

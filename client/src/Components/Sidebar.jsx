6572bacf314ee8dcfc071d33ea5ad4dd0d0b32af
Josh <herz.josh@gmail.com>
2019-11-12 17:58:38 -0800

    edited SideBar to take in props according to react-burger-menu docs

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import { slide as Menu } from "react-burger-menu";
 
 class Sidebar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    };
+  }
   render() {
     return (
-      <Menu>
-        <a className="menu-item" href="#chemvr">
+      <Menu width={ '60%' } >
+       {/* <Menu {...this.props}> */}
+        <a className="bm-name">
+        Josh Herz
+        </a>
+        <a className="bm-email" >
+        jherz.dev@gmail.com
+        </a>
+        <a className="bm-item" href="#chemvr">
         ChemVR
         </a>
         <a className="menu-item" href="#ochemxr">
         OchemXR
         </a>
-        <a className="menu-item" href="#collaborate">
-        Collaborate With Me
+        <a className="menu-item" href="#collab">
+        Collab With Me
         </a>
+        
       </Menu>
     )
   }
